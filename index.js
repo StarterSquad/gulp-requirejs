effdf67e8d4933368e92042a97cde07b416381f1
Dmitry Evseev <kradmiy@gmail.com>
2014-01-18 22:51:31 +0200

    Enables requirejs to notify when it's done

--- index.js
+++ index.js
@@ -1,56 +1,33 @@
-var gutil       = require('gulp-util'),
-    requirejs   = require('requirejs'),
-    PluginError = gutil.PluginError,
-    File        = gutil.File,
-    es          = require('event-stream')
+var gutil     = require('gulp-util'),
+  requirejs   = require('requirejs'),
+  PluginError = gutil.PluginError,
+  es          = require('event-stream');
 
 // Consts
 const PLUGIN_NAME = 'gulp-requirejs';
 
-
 module.exports = function(opts) {
+  if (!opts) {
+    throw new PluginError(PLUGIN_NAME, 'Missing options array!');
+  }
 
-    'use strict';
+  if (!opts.out && typeof opts.out !== 'string') {
+    throw new PluginError(PLUGIN_NAME, 'Only single file outputs are supported right now, please pass a valid output file name!');
+  }
 
-    if (!opts) {
-        throw new PluginError(PLUGIN_NAME, 'Missing options array!');
-    }
+  if (!opts.baseUrl) {
+    throw new PluginError(PLUGIN_NAME, 'Pipeing dirs/files is not supported right now, please specify the base path for your script.');
+  }
 
-    if (!opts.out && typeof opts.out !== 'string') {
-        throw new PluginError(PLUGIN_NAME, 'Only single file outputs are supported right now, please pass a valid output file name!');
-    }
+  return es.map(function (file, callback) {
+    var ps = es.pause();
 
-    if (!opts.baseUrl) {
-        throw new PluginError(PLUGIN_NAME, 'Pipeing dirs/files is not supported right now, please specify the base path for your script.');
-    }
-
-    // create the stream and save the file name (opts.out will be replaced by a callback function later)
-    var _s     = es.pause(),
-        _fName = opts.out;
-
-    // just a small wrapper around the r.js optimizer, we write a new gutil.File (vinyl) to the Stream, mocking a file, which can be handled
-    // regular gulp plugins (i hope...).
-    
-    // try {
-        optimize(opts, function(text) {
-            _s.write(new File({
-                path: _fName,
-                contents: new Buffer(text)
-            }));
-        });
-    // } catch (err) {
-    //     _s.emit('error', err);
-    // }
-
-    
-
-    // return the stream for chain .pipe()ing
-    return _s;
-}
-
-// a small wrapper around the r.js optimizer
-function optimize(opts, cb) {
-    opts.out = cb;
+    opts.out = function(text) {
+      file.contents = new Buffer(text);
+      ps.resume();
+      callback(null, file);
+    };
     opts.optimize = 'none';
     requirejs.optimize(opts);
-}
+  });
+};
